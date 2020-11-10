9b3f8655c073fe4ce9d52802515580b87d0c33ae
William Suppiger <suppiger@usc.edu>
2020-11-09 22:51:10 -0800

    Added more scoreboard

---
+++ dev/src/com/ums/backend/Scoreboard.java
@@ -0,0 +1,19 @@
+package com.ums.backend;
+
+public class Scoreboard {
+//	  private final long id;
+//	  private final String content;
+//
+//	  public Scoreboard(long id, String content) {
+//	    this.id = id;
+//	    this.content = content;
+//	  }
+//
+//	  public long getId() {
+//	    return id;
+//	  }
+//
+//	  public String getContent() {
+//	    return content;
+//	  }
+}
---
+++ dev/src/com/ums/backend/UMSBackend.java
@@ -0,0 +1,13 @@
+package com.ums.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMSBackend {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UMSBackend.class, args);
+  }
+
+}
