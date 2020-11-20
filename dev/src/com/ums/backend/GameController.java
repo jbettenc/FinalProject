e97fcf656e5a2100b34f553652b1625a721869c6
Jordan Bettencourt <jbettenc@usc.edu>
2020-11-20 14:41:42 -0800

    Integration Changes
    Integration changes after finding errors during testing.

--- dev/src/com/ums/backend/GameController.java
+++ dev/src/com/ums/backend/GameController.java
@@ -2,12 +2,16 @@ package com.ums.backend;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,42 +22,51 @@ import games.Game;
 import games.RPS;
 import games.TTT;
 
+@CrossOrigin
 @Controller
 public class GameController {
 	//game array
 	private static ConcurrentHashMap<String, Game> games = new ConcurrentHashMap<>();
-	private static Queue<Game> rpsGames;
-	private static Queue<Game> tttGames;
+	private static Queue<Game> rpsGames = new ConcurrentLinkedQueue<>();
+	private static Queue<Game> tttGames = new ConcurrentLinkedQueue<>();
 	private Gson gson=new GsonBuilder().setPrettyPrinting().create();
 	
-	@GetMapping("/game/join?type=${type}&email=${email}")
+	@PostMapping("/game/join/{type}/{email}")
 	@ResponseBody
-	public ResponseEntity<String> getGameID(@PathVariable String type, @PathVariable String email){
+	public ResponseEntity<JSONObject> getGameID(@PathVariable String type, @PathVariable String email){
 		Game g=null;
 		if(type.equals("rps")) {
 			if(rpsGames.isEmpty()) {
 				g=new RPS(email);
 				rpsGames.add(g);
-				return new ResponseEntity<>(g.getGameID().toString(), HttpStatus.CREATED);
+				JSONObject ret = new JSONObject();
+				ret.put("gameId", g.getGameID().toString());
+				return new ResponseEntity<>(ret, HttpStatus.CREATED);
 			}
 			else {
 				g=rpsGames.remove();
 				g.addUser(email);
 				games.put(g.getGameID().toString(), g);
-				return new ResponseEntity<>(g.getGameID().toString(), HttpStatus.ACCEPTED);
+				JSONObject ret = new JSONObject();
+				ret.put("gameId", g.getGameID().toString());
+				return new ResponseEntity<>(ret, HttpStatus.ACCEPTED);
 			}
 		}
 		else if(type.equals("ttt")) {
 			if(tttGames.isEmpty()) {
 				g=new TTT(email);
 				tttGames.add(g);
-				return new ResponseEntity<>(g.getGameID().toString(), HttpStatus.CREATED);
+				JSONObject ret = new JSONObject();
+				ret.put("gameId", g.getGameID().toString());
+				return new ResponseEntity<>(ret, HttpStatus.CREATED);
 			}
 			else {
 				g=tttGames.remove();
 				g.addUser(email);
 				games.put(g.getGameID().toString(), g);
-				return new ResponseEntity<>(g.getGameID().toString(), HttpStatus.ACCEPTED);
+				JSONObject ret = new JSONObject();
+				ret.put("gameId", g.getGameID().toString());
+				return new ResponseEntity<>(ret, HttpStatus.ACCEPTED);
 			}
 		}
 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -67,10 +80,12 @@ public class GameController {
 		return new ResponseEntity<>(gson.toJson(g), HttpStatus.ACCEPTED);
 	}
 	
-	@PutMapping("/game/{gameID}?move=${selection}&email=${email}")
+	@PutMapping("/game/{gameID}/{selection}/{email}")
 	@ResponseBody
 	public ResponseEntity<Integer> makeMove(@PathVariable String gameID, @PathVariable String selection, @PathVariable String email){
 		Game g=games.get(gameID);
+		if(g == null)
+			return new ResponseEntity<>(404, HttpStatus.NOT_FOUND);
 		int err=g.move(email, selection);
 		if(err==200) return new ResponseEntity<>(err, HttpStatus.OK);
 		else if(err==401) return new ResponseEntity<>(err, HttpStatus.UNAUTHORIZED);
