1daab70d387584da59000dcfcab55d618655aa51
William Suppiger <suppiger@usc.edu>
2020-11-21 12:10:08 -0800

    added jar for server

--- dev/src/com/ums/backend/UMSBackend.java
+++ dev/src/com/ums/backend/UMSBackend.java
@@ -1,9 +1,14 @@
 package com.ums.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class UMSBackend {
 
   public static void main(String[] args) {
